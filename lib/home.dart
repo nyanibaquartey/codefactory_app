934a6b03031f44727e882bf1d1005a5a110a9f9c
Nana Nyaniba Quartey <nyaniba.quartey@yahoo.com>
2023-01-13 16:13:18 +0000

    Home page course cards

--- lib/home.dart
+++ lib/home.dart
@@ -49,6 +49,9 @@ class Home extends StatelessWidget {
                   ),
                 ),
               ),
+              const SizedBox(
+                height: 12,
+              ),
               TextField(
                 decoration: InputDecoration(
                     hintText: "Search course",
@@ -62,6 +65,9 @@ class Home extends StatelessWidget {
                       Icons.search_rounded,
                       color: Color(0xFF3C3A36),
                     )),
+              ),
+              const SizedBox(
+                height: 12,
               ),
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
@@ -101,31 +107,115 @@ class Home extends StatelessWidget {
                   ],
                 ),
               ),
-              Column(
-                children: [
-                  Stack(
-                    children: [
-                      SizedBox(
-                        width: MediaQuery.of(context).size.width,
-                        height: 194,
-                        child: Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(12)),
-                          child: Image.asset("assets/images/home1.png"),
-                        ),
-                      ),
-                      const Align(
-                       //   alignment: Alignment.bottomRight,
-                          child: HomeCategoryCard(categoryText: "50")),
-
-                    ],
-                  ),
-                ],
+              const SizedBox(
+                height: 12,
               ),
-
+              const HomeCourseCard(
+                homeCourseCardImage: "assets/images/home1.png",
+                courseDuration: "3 h 30 min",
+                courseName: "UI",
+                courseDesc: "Advanced mobile interface design",
+              ),
+              const SizedBox(
+                height: 12,
+              ),
+              const HomeCourseCard(
+                homeCourseCardImage: "assets/images/home2.png",
+                courseDuration: "3 h 30 min",
+                courseName: "HTML",
+                courseDesc: "Advanced web applications",
+              ),
             ],
           ),
         )),
+      ),
+    );
+  }
+}
+
+class HomeCourseCard extends StatelessWidget {
+  final String homeCourseCardImage;
+  final String courseDuration;
+  final String courseName;
+  final String courseDesc;
+
+  const HomeCourseCard({
+    Key? key,
+    required this.homeCourseCardImage,
+    required this.courseDuration,
+    required this.courseName,
+    required this.courseDesc,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width,
+      height: 297,
+      child: Card(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8),
+            side: BorderSide(color: Color(0xFFBEBAB3))),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Stack(
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: 138,
+                  child: Image.asset(homeCourseCardImage),
+                ),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 16, right: 16, bottom: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Align(
+                      alignment: Alignment.bottomRight,
+                      child: HomeCategoryCard(categoryText: "50")),
+                  Text(
+                    courseDuration,
+                    style: GoogleFonts.rubik(
+                      textStyle: Theme.of(context).textTheme.bodySmall,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 12.0,
+                      color: const Color(0xFF5BA092),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    courseName,
+                    style: GoogleFonts.rubik(
+                      textStyle: Theme.of(context).textTheme.headline4,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 24.0,
+                      color: const Color(0xFF3C3A36),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    courseDesc,
+                    style: GoogleFonts.rubik(
+                      textStyle: Theme.of(context).textTheme.bodyMedium,
+                      fontWeight: FontWeight.w400,
+                      fontSize: 14.0,
+                      color: const Color(0xFF3C3A36),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -143,17 +233,21 @@ class HomeCategoryCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: 70,
+      height: 25,
       child: Card(
         color: const Color(0xFF65AAEA),
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-        child: Text(
-          categoryText,
-          textAlign: TextAlign.center,
-          style: GoogleFonts.rubik(
-            textStyle: Theme.of(context).textTheme.labelSmall,
-            fontWeight: FontWeight.w500,
-            fontSize: 12.0,
-            color: const Color(0xFFF2F2F2),
+        child: Align(
+          alignment: Alignment.center,
+          child: Text(
+            categoryText,
+            textAlign: TextAlign.center,
+            style: GoogleFonts.rubik(
+              textStyle: Theme.of(context).textTheme.labelSmall,
+              fontWeight: FontWeight.w500,
+              fontSize: 12.0,
+              color: const Color(0xFFF2F2F2),
+            ),
           ),
         ),
       ),
