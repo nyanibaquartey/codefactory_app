66f6744bffa0a903ccf6b33e8396d0520b8afe49
Nana Nyaniba Quartey <nyaniba.quartey@yahoo.com>
2023-01-25 13:51:04 +0000

    Course Tests page initial commit

---
+++ lib/course_tests.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CourseTests extends StatelessWidget {
+  const CourseTests({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
