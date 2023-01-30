4de168653b691e912b207ba089d295add679b666
Nana Nyaniba Quartey <nyaniba.quartey@yahoo.com>
2023-01-30 16:16:26 +0000

    Initial Commit for second Results page

---
+++ lib/results2.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Results2 extends StatelessWidget {
+  const Results2({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
