42ca224c1bc5d66664ed48a15b1efc993e3cfe7b
Nana Nyaniba Quartey <nyaniba.quartey@yahoo.com>
2023-03-13 12:10:58 +0000

    Route to splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,7 @@
-import 'package:codefactory_app/course_lesson.dart';
-import 'package:codefactory_app/course_not_saved.dart';
-import 'package:codefactory_app/course_tests.dart';
-import 'package:codefactory_app/no_payment.dart';
-import 'package:codefactory_app/course_not_found.dart';
-import 'package:codefactory_app/onboarding_screen.dart';
-import 'package:codefactory_app/payment.dart';
-import 'package:codefactory_app/product_detail.dart';
-import 'package:codefactory_app/results.dart';
-import 'package:codefactory_app/results2.dart';
-import 'package:codefactory_app/settings.dart';
-import 'package:codefactory_app/test_question.dart';
-import 'package:codefactory_app/your_courses.dart';
+
 import 'package:flutter/material.dart';
 import 'package:codefactory_app/splash_screen.dart';
-import 'package:codefactory_app/home.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -26,9 +14,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Code Factory',
 theme: ThemeData.light(),
-      home: const OnboardingScreen(),
+      home: const SplashScreen(),
     );
   }
 }
